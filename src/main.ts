42671b8fbbd271cbaa93683ca57bd268e9177248
Elvis <elvis.adomnica@gmail.com>
2024-01-01 13:21:36 +0100

    Cleanup code

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import "./style.css";
 import Emulator from "./lib/emulator.ts";
-import { BitMapDisplay, TextDisplay } from "./lib/display.ts";
-import { circularArray, fetchRom, uploadRom } from "./utils.ts";
+import { BitMapDisplay } from "./lib/display.ts";
+import { fetchRom } from "./utils.ts";
 
 const display = new BitMapDisplay(
   document.getElementById("display")! as HTMLCanvasElement
 );
-const info = new TextDisplay(document.getElementById("info")!);
-
 const emulator = new Emulator();
-const romList = circularArray([
-  { name: "ibm-logo.ch8", data: await fetchRom("ibm-logo.ch8") },
-  { name: "test-opcode.ch8", data: await fetchRom("test-opcode.ch8") },
-]);
-
 const qKeyboardMapping: any = {
   "1": 0x1,
   "2": 0x2,
@@ -40,39 +33,23 @@ function drawDisplay(emulator: Emulator) {
     emulator.framebufferWidth,
     emulator.framebufferHeight
   );
-
-  info.clear();
-  info.write(`ROM: ${emulator.romDisk.name}\n`);
 }
 
-document.addEventListener("keydown", async (e) => {
-  const hex = qKeyboardMapping[e.key];
-  if (hex) {
-    emulator.setInput(hex);
-  }
+async function main(fileName: string) {
+  document.addEventListener("keydown", async (e) => {
+    const hex = qKeyboardMapping[e.key];
+    if (hex) {
+      emulator.setInput(hex);
+    }
+  });
 
-  switch (e.keyCode) {
-    case 13: // Enter
-      emulator.load(romList.peek());
-      break;
-    case 78: // N
-      emulator.load(romList.next());
-      break;
-    case 80: // P
-      emulator.load(romList.prev());
-      break;
-    case 84: // T
-      emulator.tick();
-      break;
-    case 85: // U
-      const rom = await uploadRom();
-      emulator.load(romList.add(rom));
-      break;
-    default:
-  }
+  // main loop
+  setInterval(() => {
+    emulator.tick();
+    drawDisplay(emulator);
+  }, 0);
 
-  drawDisplay(emulator);
-});
+  emulator.load({ name: fileName, data: await fetchRom(fileName) });
+}
 
-// start with first element
-emulator.load(romList.next());
+main("ibm-logo.ch8");
